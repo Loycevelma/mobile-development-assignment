c2babe37e18345b6bb4016dd370aef496f80441d
loycevelma <amondiloyce@gmail.com>
2021-02-20 23:51:34 +0300

    first commit

---
+++ src/velma3.kt
@@ -0,0 +1,17 @@
+fun main() {
+    val name ="loyce velma amondi"
+    println(name)
+
+    var age=18
+    println(age)
+
+    val phoneNumber="0706387923"
+    println(phoneNumber)
+
+    var weight :Int = 59
+    println(weight)
+
+    val citizen :String ="kenya"
+    println(citizen)
+
+}
